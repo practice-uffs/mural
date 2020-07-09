c96d78b161679c7810926f913a29e12246128c29
Arufonsekun <junior.ramisch@gmail.com>
2020-07-09 19:38:23 -0300

    feat(layout): updates design to match material

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -15,49 +15,59 @@
     <body>
         <div class="container-fluid">
             <div class="row justify-content-center">
-                <div class="col-xs-12 col-md-5 col-xl-4">
+                <div class="col-xs-12 col-sm-8 col-md-5 col-xl-4">
                     <div class="card mt-5">
                         <div class="card-body">
-                            <img src="https://practice.uffs.cc/images/logo.png" alt="practice-logo" class="site__logo">
 
-            <h2>Autenticação</h2>
-
+                            <div class="card-media">
+                                <img src="https://practice.uffs.cc/images/logo.png" alt="practice-logo" class="site__logo">
+                            </div>
+                            
                             <form action="" method="post" class="form">
                                 @csrf
 
                                 <div class="form__field">
-                                    <label for="username">
+                                    <label for="username" class="form__label">
                                         idUFFS
                                     </label>
-                                    <input id="username" type="text" class="@error('username') is-invalid @enderror" 
-                                        name="username" value="{{ old('username') }}" placeholder="Ex. fulano.silva" 
-                                        required autofocus
+                                    <input id="username" type="text" name="username" value="{{ old('username') }}"
+                                           placeholder="Ex. fulano.silva" required autofocus autocomplete="off"
+                                           class="@error('username') is-invalid @enderror field__input shadow-focus" 
                                     >
                                 </div>
 
                                 <div class="form__field">
-                                    <label for="password">
+                                    <label for="password" class="form__label">
                                         Senha
                                     </label>
                                     <input id="password" type="password" 
-                                        class="@error('password') is-invalid @enderror" name="password"
-                                        placeholder="Digite sua senha" required autocomplete="current-password"
+                                           class="@error('password') is-invalid @enderror field__input shadow-focus mb-4"
+                                           name="password" placeholder="Digite sua senha" required autocomplete="current-password"
                                     >
                                 </div>
 
                                 <div class="form__field">
-                                    <input type="submit" value="Login" class="field__submit">
+                                    <input type="submit" value="Login" class="btn field__submit shadow-focus">
                                 </div>
                             </form>
 
-            @if ($errors->any()) 
-            <div class="alert alert-danger" role="alert">
-                @foreach ($errors->all() as $error)
-                    {{ $error }}
-                @endforeach
+                            @if ($errors->any()) 
+                            <div class="alert alert-danger" role="alert">
+                                @foreach ($errors->all() as $error)
+                                    {{ $error }}
+                                @endforeach
+                            </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+
+                <div class="banner">
+                    <img src="{{ asset('img/banner-practice.png') }}" 
+                        alt="practice-banner" class="banner__img"
+                    >
+                </div>
             </div>
-            @endif
         </div>
-    </div>
-</body>
+    </body>
 </html>
