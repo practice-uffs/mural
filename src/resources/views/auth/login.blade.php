4ab6281c7f5f6dbd1fd4074ee01641361fa34dfd
Arufonsekun <junior.ramisch@gmail.com>
2020-07-09 17:36:55 -0300

    feat(layout): uses bootstrap responsive styles

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -12,38 +12,43 @@
         <link type="text/css" rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
 
-<body class="align">
-    <div class="grid align__item">
-        <div class="login">
-            <svg xmlns="http://www.w3.org/2000/svg" class="site__logo" width="56" height="84" viewBox="77.7 214.9 274.7 412">
-                <defs>
-                    <linearGradient id="a" x1="0%" y1="0%" y2="0%">
-                        <stop offset="0%" stop-color="#8ceabb" />
-                        <stop offset="100%" stop-color="#378f7b" />
-                    </linearGradient>
-                </defs>
-                <path fill="url(#a)" d="M215 214.9c-83.6 123.5-137.3 200.8-137.3 275.9 0 75.2 61.4 136.1 137.3 136.1s137.3-60.9 137.3-136.1c0-75.1-53.7-152.4-137.3-275.9z" />
-            </svg>
+    <body>
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-xs-12 col-md-5 col-xl-4">
+                    <div class="card mt-5">
+                        <div class="card-body">
+                            <img src="https://practice.uffs.cc/images/logo.png" alt="practice-logo" class="site__logo">
 
             <h2>Autenticação</h2>
 
-            <form action="" method="post" class="form">
-                @csrf
+                            <form action="" method="post" class="form">
+                                @csrf
 
-                <div class="form__field">
-                    <label for="username">idUFFS</label>
-                    <input id="username" type="text" class="@error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" placeholder="Ex. fulano.silva" required autofocus>
-                </div>
+                                <div class="form__field">
+                                    <label for="username">
+                                        idUFFS
+                                    </label>
+                                    <input id="username" type="text" class="@error('username') is-invalid @enderror" 
+                                        name="username" value="{{ old('username') }}" placeholder="Ex. fulano.silva" 
+                                        required autofocus
+                                    >
+                                </div>
 
-                <div class="form__field">
-                    <label for="password">Senha</label>
-                    <input id="password" type="password" class="@error('password') is-invalid @enderror" name="password" placeholder="Digite sua senha" required autocomplete="current-password">
-                </div>
+                                <div class="form__field">
+                                    <label for="password">
+                                        Senha
+                                    </label>
+                                    <input id="password" type="password" 
+                                        class="@error('password') is-invalid @enderror" name="password"
+                                        placeholder="Digite sua senha" required autocomplete="current-password"
+                                    >
+                                </div>
 
-                <div class="form__field">
-                    <input type="submit" value="Login">
-                </div>
-            </form>
+                                <div class="form__field">
+                                    <input type="submit" value="Login" class="field__submit">
+                                </div>
+                            </form>
 
             @if ($errors->any()) 
             <div class="alert alert-danger" role="alert">
