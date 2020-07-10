0c5042bb1897748ce3ca002142f3b030ae3124d4
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-07-10 13:59:17 -0300

    chore: Change dashboard files location.

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -7,22 +7,22 @@
     <meta name=robots content=noindex>
     <!--[if IE]><link rel="icon" href="favicon.png"><![endif]-->
     <title>homer</title>
-    <link href="{{ asset('homer/css/app.3b90d21f.css') }}" rel=preload as=style>
-    <link href="{{ asset('homer/css/chunk-vendors.49f69c55.css') }}" rel=preload as=style>
-    <link href="{{ asset('homer/js/app.af4239f3.js') }}" rel=preload as=script>
-    <link href="{{ asset('homer/js/chunk-vendors.06bf9d4a.js') }}" rel=preload as=script>
-    <link href="{{ asset('homer/css/chunk-vendors.49f69c55.css') }}" rel=stylesheet>
-    <link href="{{ asset('homer/css/app.3b90d21f.css') }}" rel=stylesheet>
-    <link rel=icon type=image/png sizes=32x32 href="{{ asset('homer/assets/icons/favicon-32x32.png') }}">
-    <link rel=icon type=image/png sizes=16x16 href="{{ asset('homer/assets/icons/favicon-16x16.png') }}">
-    <link rel=manifest href="{{ asset('homer/assets/manifest.json') }}">
+    <link href="{{ asset('css/app.3b90d21f.css') }}" rel=preload as=style>
+    <link href="{{ asset('css/chunk-vendors.49f69c55.css') }}" rel=preload as=style>
+    <link href="{{ asset('js/app.af4239f3.js') }}" rel=preload as=script>
+    <link href="{{ asset('js/chunk-vendors.06bf9d4a.js') }}" rel=preload as=script>
+    <link href="{{ asset('css/chunk-vendors.49f69c55.css') }}" rel=stylesheet>
+    <link href="{{ asset('css/app.3b90d21f.css') }}" rel=stylesheet>
+    <link rel=icon type=image/png sizes=32x32 href="{{ asset('assets/icons/favicon-32x32.png') }}">
+    <link rel=icon type=image/png sizes=16x16 href="{{ asset('assets/icons/favicon-16x16.png') }}">
+    <link rel=manifest href="{{ asset('assets/manifest.json') }}">
     <meta name=theme-color content=#4DBA87>
     <meta name=apple-mobile-web-app-capable content=yes>
     <meta name=apple-mobile-web-app-status-bar-style content=black>
     <meta name=apple-mobile-web-app-title content="Homer Dashboard">
-    <link rel=apple-touch-icon href="{{ asset('homer/assets/icons/apple-touch-icon-152x152.png') }}">
-    <link rel=mask-icon href="{{ asset('homer/assets/icons/safari-pinned-tab.svg') }}" color=#4DBA87>
-    <meta name=msapplication-TileImage content="{{ asset('homer/assets/icons/msapplication-icon-144x144.png') }}">
+    <link rel=apple-touch-icon href="{{ asset('assets/icons/apple-touch-icon-152x152.png') }}">
+    <link rel=mask-icon href="{{ asset('assets/icons/safari-pinned-tab.svg') }}" color=#4DBA87>
+    <meta name=msapplication-TileImage content="{{ asset('assets/icons/msapplication-icon-144x144.png') }}">
     <meta name=msapplication-TileColor content=#000000>
 </head>
 <body>
@@ -30,7 +30,7 @@
         <strong>We're sorry but homer doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
     </noscript>
     <div id=app></div>
-    <script src="{{ asset('homer/js/chunk-vendors.06bf9d4a.js') }}"></script>
-    <script src="{{ asset('homer/js/app.af4239f3.js') }}"></script>
+    <script src="{{ asset('js/chunk-vendors.06bf9d4a.js') }}"></script>
+    <script src="{{ asset('js/app.af4239f3.js') }}"></script>
 </body>
 </html>
