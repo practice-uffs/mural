21a83479c07fa634dea133e759c98e6bdc99d925
Arufonsekun <junior.ramisch@gmail.com>
2020-07-13 22:46:29 -0300

    feat(login): adds transparent banner by @estelavilasboas
    also changes default font-family to Roboto

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -11,7 +11,6 @@
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
         <link type="text/css" rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}"/>
         
         <!-- Styles -->
