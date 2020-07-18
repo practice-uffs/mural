b73d6cafd7297d33fd6cf3ae924ff0077de454bc
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-07-18 15:16:00 -0300

    feat: Add reaction's section.

--- src/resources/views/item/show.blade.php
+++ src/resources/views/item/show.blade.php
@@ -1,124 +1,76 @@
-@extends('layouts.main')
-
-@section('headsup')
-
-<div class="row">
-    <div class="col-lg-12">
-        <div class="alert alert-danger" role="alert">
-            <h4 class="alert-heading">Atenção</h4>
-            <p>Esse horário é preliminar. Ele foi decidido e homologado pelo Colegiado do Curso, então há baixas chances que ele seja alterado. Entretanto, ele pode sofrer alterações conforme demandas da Coordenação Acadêmica em virtude de alocações fora do controle da Coordenação do Curso.</p>
-            <hr>
-            <p class="mb-0">Dúvidas? Escreva para <a href="mailto:computacao.ch@uffs.edu.br">computacao.ch@uffs.edu.br</a></p>
-        </div>
-    </div>
-</div>
-
-@endsection
+@extends('layouts.app')
 
 @section('content')
 
-<div class="row">
-    <div class="col-lg-12">
-        <div class="card text-white bg-dark border-secondary">
-            <div class="card-header">
-                Informações
+<header>
+    <nav class="nav-extended">
+        <div class="nav-wrapper grey lighten-5">
+            <a href="#" class="brand-logo">
+                <img src="{{ asset('img/logo-practice.png') }}" class="nav__img">
+            </a>
+            <ul id="nav-mobile" class="right">
+
+            </ul>
+        </div>
+
+        <div class="nav-content grey lighten-2">
+
+        </div>
+    </nav>
+</header>
+
+<main class="container">
+    <div class="card">
+        <div class="card-content">
+            <div class="card-title">
+                {{ $item -> title }}
             </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-6">
-                        <p class="text-muted">Título/tema</p>
-                        <p class="">Uso de Realidade Aumentada para auxiliar o ensino de Geometria na disciplina de Computação Gráfica na UFFS</p>
-                    </div>
-                    <div class="col-2">
-                        <p class="text-muted">Tipo</p>
-                        <p class="">Projeto (TCC I)</p>
-                    </div>
-                    <div class="col-2">
-                        <p class="text-muted">Período</p>
-                        <p class="">2019/2</p>
-                    </div>
-                    <div class="col-2">
-                        <p class="text-muted">Situação</p>
-                        <p class="">Em andamento</p>
-                    </div>
-                </div>
 
-                <div class="row section">
-                    <div class="col-12">
-                        <p class="text-muted">Resumo</p>
-                        <p class="text-muted small text-justify">A associação dos conceitos matemáticos com a realidade é um desafio para a maioria dos
+            <p>{{ $item -> description }}</p>
 
-estudantes. A tecnologia atual fornece ferramentas para criar opções que aproximam essa per-
-cepção. À vista disso, essa pesquisa mostra o potencial que a Realidade Aumentada tem quando
+        </div>
 
-associada a Teoria da Carga Cognitiva e a Visualização, na elaboração de uma ferramenta que
-auxilie os estudantes na concepção e aprendizado de conceitos matemáticos que são utilizados
-
-na disciplina de Computação Gráfica na UFFS. São apresentados os referenciais teóricos em-
-pregados nessa pesquisa, bem como o processo de criação do protótipo de uma aplicação de
-
-Realidade Aumentada contendo demonstrações sobre vetores em uma aula de Computação Grá-
-fica. A pesquisa demonstra a importância da Realidade Aumentada como auxílio nos processos
-
-práticos pedagógicos, através da avaliação das respostas e opiniões coletadas na fase de testes e
-validação do protótipo desenvolvido.</p>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-4 text-left">
-                        <div class="container-fluid">
-                            <div class="row align-items-center">
-                                <div class="col-2">
-                                    <img class="user-avatar rounded-circle" src="https://colorlib.com/polygon/sufee/images/admin.jpg" alt="User Avatar">
-                                </div>
-                                <div class="col-10">
-                                    <p>Fernando Bevilacqua <br/><small class="text-muted">fernando.bevilacqua</small></p>
-                                </div>
-                            </div>
-                        </div>
+        <div class="card__reaction">
+            <ul class="reaction-list">
+                <li class="reaction">
+                    <div class="reaction__icon">
+                        <i class="material-icons">error_outline</i>
                     </div>
 
-                    <div class="col-4 text-left">
-                        <div class="container-fluid">                        
-                            <div class="row align-items-center">
-                                <div class="col-2">
-                                    <img alt="image" class="user-avatar rounded-circle" src="https://colorlib.com/polygon/sufee/images/admin.jpg">
-                                </div>
-                                <div class="col-10">
-                                    <p>
-                                        Fernando Bevilacqua <br/>
-                                        <small class="text-muted">fernando.bevilacqua</small><br />
-                                        <span class="badge badge-danger">Não confirmado</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
+                    <div class="reaction__count">
+                        5
+                    </div>
+                </li>
+
+                <li class="reaction">
+                    <div class="reaction__icon">
+                        <i class="material-icons">add</i>
                     </div>
 
-                    <div class="col-4 text-left">
-                        <div class="container-fluid">                        
-                            <div class="row align-items-center">
-                                <div class="col-2">
-                                    <img alt="image" class="user-avatar rounded-circle" src="https://colorlib.com/polygon/sufee/images/admin.jpg">
-                                </div>
-                                <div class="col-10">
-                                    <p>
-                                        Fernando Bevilacqua <br/>
-                                        <small class="text-muted">fernando.bevilacqua</small><br />
-                                        <span class="badge badge-danger">Não confirmado</span>
-                                    </p>
-                                </div>
-                            </div> 
-                        </div>
+                    <div class="reaction__count">
+                        8
                     </div>
-                </div>
-            </div>
+                </li>
+
+                <li class="reaction">
+                    <div class="reaction__icon">
+                        <i class="material-icons">phone</i>
+                    </div>
+
+                    <div class="reaction__count">
+                        2
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
-</div>
 
-<div class="row">
+    <div class="timeline">
+
+    </div>
+</main>
+
+{{-- <div class="row">
     <div class="col-md-12 col-lg-12">
         <div id="timeline-pre"></div>
         <div id="timeline">
@@ -147,6 +99,67 @@ validação do protótipo desenvolvido.</p>
             </div>
         </div>
     </div>
+</div> --}}
+
+<div class="banner">
+    <img src="{{ asset('img/banner-practice.png') }}"
+        alt="practice-banner" class="banner__img"
+    >
 </div>
+
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h4 class="white-text">Web Feedback</h4>
+                <p class="grey-text text-lighten-4">
+                    Web Feedback é um projeto que objetiva estreitar a comunicação
+                    entre a comunidade UFFS e o programa Practice. Com ele é possível
+                    que você publique seus comentários, sugestões e ideias voltadas para
+                    a melhoria do ensino e da educação para que assim o Practice tenha
+                    facilidade no planejamento e execução de seus projetos.
+                </p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Saiba mais</h5>
+                <ul>
+                    <li>
+                        <a class="grey-text text-lighten-3" href="https://practice.uffs.cc/sobre/" target="blank">
+                            Sobre
+                        </a>
+                    </li>
+                    <li>
+                        <a class="grey-text text-lighten-3" href="https://practice.uffs.cc/equipe/" target="blank">
+                            Quem somos
+                        </a>
+                    </li>
+                    <li>
+                        <a class="grey-text text-lighten-3" href="https://practice.uffs.cc/parcerias" target="blank">
+                            Parcerias
+                        </a>
+                    </li>
+                    <li>
+                        <a class="grey-text text-lighten-3" href="https://github.com/practice-uffs" target="blank">
+                            Github
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            <p class="grey-text text-lighten-4 left">
+                © 2020 All rights reserved.
+            </p>
+            <p class="grey-text text-lighten-4 right">
+                Powered by
+                <a class="grey-text text-lighten-4" href="htpps://materializecss.com">
+                MaterializeCSS
+            </a>
+            </p>
+        </div>
+    </div>
+</footer>
 
 @endsection
