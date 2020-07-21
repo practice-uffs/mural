e26eb6e898752317aff5640da28a70d20b40630c
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-07-21 17:26:06 -0300

    feat: creat commment list component.

---
+++ src/resources/js/components/CommentListComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div :class="['timeline',
+        {'timeline--with-comment': userCommented}
+    ]">
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            comments: {},
+        }
+    },
+
+    props: {
+        'userCommented': Boolean,
+    },
+
+    methods: {
+        list() {
+            
+        }
+    }
+}
+</script>
