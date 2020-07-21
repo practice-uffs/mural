16714359aa25a3d18ef7313fcaf8544013d52c9d
Arufonsekun <junior.ramisch@gmail.com>
2020-07-21 16:30:26 -0300

    style: set views hierarchy
     - app.blade is the default view
     - footer is present in all views
     - header is optional

--- src/resources/views/item/create.blade.php
+++ src/resources/views/item/create.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
-@section('content')
-
+@section('header')
 <header>
     <nav class="nav-extended">
         <div class="nav-wrapper grey lighten-5">
@@ -18,7 +17,9 @@
         </div>
     </nav>
 </header>
+@endsection
 
+@section('content')
 <main class="container">
     <div class="row">
         <div class="col l12">
@@ -139,65 +140,4 @@
         </div>
     </div>
 </main>
-
-<div class="banner">
-    <img src="{{ asset('img/banner-practice.png') }}"
-        alt="practice-banner" class="banner__img"
-    >
-</div>
-
-<footer class="page-footer">
-    <div class="container">
-        <div class="row">
-            <div class="col l6 s12">
-                <h4 class="white-text">Web Feedback</h4>
-                <p class="grey-text text-lighten-4">
-                    Web Feedback é um projeto que objetiva estreitar a comunicação
-                    entre a comunidade UFFS e o programa Practice. Com ele é possível
-                    que você publique seus comentários, sugestões e ideias voltadas para
-                    a melhoria do ensino e da educação para que assim o Practice tenha
-                    facilidade no planejamento e execução de seus projetos.
-                </p>
-            </div>
-            <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Saiba mais</h5>
-                <ul>
-                    <li>
-                        <a class="grey-text text-lighten-3" href="https://practice.uffs.cc/sobre/" target="blank">
-                            Sobre
-                        </a>
-                    </li>
-                    <li>
-                        <a class="grey-text text-lighten-3" href="https://practice.uffs.cc/equipe/" target="blank">
-                            Quem somos
-                        </a>
-                    </li>
-                    <li>
-                        <a class="grey-text text-lighten-3" href="https://practice.uffs.cc/parcerias" target="blank">
-                            Parcerias
-                        </a>
-                    </li>
-                    <li>
-                        <a class="grey-text text-lighten-3" href="https://github.com/practice-uffs" target="blank">
-                            Github
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <div class="footer-copyright">
-        <div class="container">
-            <p class="grey-text text-lighten-4 left">
-                © 2020 All rights reserved.
-            </p>
-            <p class="grey-text text-lighten-4 right">
-                Powered by
-                <a class="grey-text text-lighten-4" href="htpps://materializecss.com">
-                MaterializeCSS
-            </a>
-            </p>
-        </div>
-    </div>
-</footer>
 @endsection
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -18,9 +18,75 @@
     </head>
 
     <body>
+        
+        @yield('header')
+
         <div id="app">
             @yield('content')
         </div>
+
+        <!-- Practice Banner -->
+        <div class="banner">
+            <img src="{{ asset('img/banner-practice.png') }}"
+                alt="practice-banner" class="banner__img"
+            >
+        </div>
+
+        <!-- WebFeedback Footer -->
+        <footer class="page-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col l6 s12">
+                        <h4 class="white-text">Web Feedback</h4>
+                        <p class="grey-text text-lighten-4">
+                            Web Feedback é um projeto que objetiva estreitar a comunicação
+                            entre a comunidade UFFS e o programa Practice. Com ele é possível
+                            que você publique seus comentários, sugestões e ideias voltadas para
+                            a melhoria do ensino e da educação para que assim o Practice tenha 
+                            facilidade no planejamento e execução de seus projetos.
+                        </p>
+                    </div>
+                    <div class="col l4 offset-l2 s12">
+                        <h5 class="white-text">Saiba mais</h5>
+                        <ul>
+                            <li>
+                                <a class="grey-text text-lighten-3" href="https://practice.uffs.cc/sobre/" target="blank">
+                                    Sobre
+                                </a>
+                            </li>
+                            <li>
+                                <a class="grey-text text-lighten-3" href="https://practice.uffs.cc/equipe/" target="blank">
+                                    Quem somos 
+                                </a>
+                            </li>
+                            <li>
+                                <a class="grey-text text-lighten-3" href="https://practice.uffs.cc/parcerias" target="blank">
+                                    Parcerias
+                                </a>
+                            </li>
+                            <li>
+                                <a class="grey-text text-lighten-3" href="https://github.com/practice-uffs" target="blank">
+                                    Github
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright">
+                <div class="container">
+                    <p class="grey-text text-lighten-4 left">
+                        © 2020 All rights reserved.
+                    </p>
+                    <p class="grey-text text-lighten-4 right">
+                        Powered by
+                        <a class="grey-text text-lighten-4" href="htpps://materializecss.com">
+                        MaterializeCSS
+                    </a>
+                    </p>
+                </div>
+            </div>
+        </footer>
 
         <script src="{{ asset('js/app.js') }}" defer></script>
     </body>
