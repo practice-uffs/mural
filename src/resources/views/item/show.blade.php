fe5a73c7f3bdc078550c09379dc79018b3c85544
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-07-23 22:48:39 -0300

    feat: add new header in views.

--- src/resources/views/item/show.blade.php
+++ src/resources/views/item/show.blade.php
@@ -3,18 +3,25 @@
 @section('content')
 
 <header>
-    <nav class="nav-extended">
-        <div class="nav-wrapper grey lighten-5">
-            <a href="#" class="brand-logo">
+    <nav class="header">
+        <div class="header__wrapper">
+            <a href="#" class="header__logo">
                 <img src="{{ asset('img/logo-practice.png') }}" class="nav__img">
             </a>
-            <ul id="nav-mobile" class="right">
 
-            </ul>
-        </div>
+            <div class="header__nav">
+                <a class="nav__link dropdown-trigger"
+                    data-target="userMenu"
+                >
+                    <i class="material-icons">person</i>{{ $user -> uid }}
+                </a>
 
-        <div class="nav-content grey lighten-2">
-
+                <ul id="userMenu" class="dropdown-content">
+                    <li>
+                        <a href="{{ route('logout') }}" class="text-primary">Sair</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 </header>
