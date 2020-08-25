3749e966e2603bdacb338c891b24073caa270de6
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-08-25 20:20:02 -0300

    feat: improves service resource

--- app/Http/Resources/ServiceResource.php
+++ app/Http/Resources/ServiceResource.php
@@ -4,6 +4,8 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
+use App\Specification;
+
 class ServiceResource extends JsonResource
 {
     /**
@@ -14,6 +16,24 @@ class ServiceResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        $specification = json_decode(
+            Specification::find($this->specification_id)
+                ->content
+        );
+
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'location_id' => $this->location_id,
+            'category_id' => $this->category_id,
+            'specification' =>$specification,
+            'status' => $this->status,
+            'type' => $this->type,
+            'title' => $this->title,
+            'description' => $this->description,
+            'hidden' => $this->hidden,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
     }
 }
