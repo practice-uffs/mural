efc4e8eadcce1649ddf5c879133855a635947a36
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-08-30 23:55:51 -0300

    chore: change header layout

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,54 @@
+<header>
+    <nav class="header nav-extended">
+        <div class="header__wrapper">
+            <a href="{{ route('items.index') }}"
+                class="header__logo"
+            >
+                <img src="{{ asset('img/logo-practice.png') }}" class="nav__img">
+            </a>
+
+            <div class="header__auth">
+                @if (Auth::check())
+                    <div class="header__user-info mr-2 pt-3">
+                        <div>
+                            {{ $user->uid }}
+                        </div>
+                        <div>
+                            {{ $user->name }}
+                        </div>
+                    </div>
+                    <a class="nav__link dropdown-trigger" data-target="userMenu">
+                        <img src="{{asset('img/avatars/avatar-' . ($user->id % 4 + 1) . '.png') }}" 
+                            class="header__avatar" alt="Avatar"
+                        >
+                @else
+                    <a href="{{ route('login') }}"
+                        class="nav__link"
+                    >
+                        <i class="material-icons">login</i> Entrar
+                @endif
+                </a>
+            
+                <ul id="userMenu" class="dropdown-content">
+                    <li>
+                        <a href="{{ route('logout') }}" class="text-primary">Sair</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        @if ($home ?? false)
+            <div class="header__nav">
+                <ul class="header__nav-links">
+                    <li class="header__nav-item">
+                        <a href="#" id="viewTrigger">
+                            <span class="material-icons">
+                                view_list
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        @endif
+    </nav>
+</header>
