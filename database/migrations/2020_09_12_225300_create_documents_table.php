1bf4a5cd49122c9647a806ef49f98ae8f949e357
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-09-12 20:20:29 -0300

    feat: creates 'Document' model

---
+++ database/migrations/2020_09_12_225300_create_documents_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('item_id')->constrained();
+
+            $table->string('name', 256);
+            $table->string('path', 256);
+            $table->string('mime_type', 32);
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
