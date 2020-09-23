18ae31d0a33a73d0fc8822b83deb39d8932b43db
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-09-22 22:31:33 -0300

    feat: adds basic Document CRUD.

--- app/Document.php
+++ app/Document.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Document extends Model
 {
+    protected $fillable = [
+        'name', 'path', 'mime_type', 'description', 'item_id'
+    ];
+
     public function item()
     {
         return $this->belongsTo('App\Item');
