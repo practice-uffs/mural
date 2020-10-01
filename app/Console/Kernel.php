15a572e81733ce9209dfafd74e2297432fffd89a
Jean Carlo Hilger <jean.hilger@2adv.com.br>
2020-10-01 13:40:06 -0300

    feat: adds basic session start
    NOTE: nothing working, things will get changed.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,8 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+use Illuminate\Support\Facades\DB;
+
 class Kernel extends ConsoleKernel
 {
     /**
@@ -24,7 +26,14 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        /**
+         * Deletes every entry in 'sessions' table that has
+         */
+        DB::table('sessions') -> where(
+            'created_at', 
+            '<', 
+            Carbon::now()->subDays(2)->toDateTimeString()
+        );
     }
 
     /**
---
+++ database/migrations/0000000000_create_sessions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')->constrained();
+
+            $table->string('token', 128);
+            $table->string('device', 256);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
