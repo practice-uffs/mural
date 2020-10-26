b18794823b661263238c5c88b4c5c10cab996c76
Raphael Borges <rah-borges@hotmail.com>
2020-10-26 09:29:27 -0300

    (feat):component Menu

---
+++ resources/js/components/layouts/MenuComponent.vue
@@ -0,0 +1,9 @@
+<template >
+<div class="nav-scroller py-1 mb-2 sticky-top bg-light">
+    <nav class="nav d-flex justify-content-around">
+        <a class="p-2 text-muted" href="#">Home</a>
+        <a class="p-2 text-muted" href="#">Feedbacks</a>
+        <a class="p-2 text-muted" href="#">Serviços</a>
+    </nav>
+</div>
+</template>
