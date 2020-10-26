a981e6ab25d7c82e4c98e04b7a26e0c8876b19b3
Raphael Borges <rah-borges@hotmail.com>
2020-10-26 09:31:33 -0300

    (feat):component Serviços para o index

---
+++ resources/js/components/home/Services.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="mt-5">     
+    <h2 class="text-center">Nossos serviços</h2>
+    <div class="mt-2 row text-center justify-content-around">
+        <div v-bind:key="service.id" v-for="service in services" class="col-sm-12 col-md-4 m-3">
+            <h3>{{service.title}}</h3>
+            <p>{{service.description}}</p>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name:"services",
+    data(){
+        return{
+            services:[
+                {
+                    id:1,
+                    title:'Criação de Funcionalidades',
+                    description:'Este serviço é ideal para a criação de novas funcionalidades'
+                },
+                {
+                    id:2,
+                    title:'Modificações e Atualizações',
+                    description:'Escontrou algum erro? Necessita algum melhoria? Este é o espaço para isso.'
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
