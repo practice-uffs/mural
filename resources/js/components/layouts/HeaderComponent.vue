1e4f2af4f86ff355379472ac2597a96b8325eeeb
Raphael Borges <rah-borges@hotmail.com>
2020-10-26 09:29:43 -0300

    (feat):component Header

---
+++ resources/js/components/layouts/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class=''>
+        <div class="container">
+            <h1>Seja Bem-vinde ao</h1>
+            <p>Portal de serviçoes do <strong>Practice</strong></p>
+        </div>
+
+        <!-- Banner separador -->    
+        <img :src="banner" width="100%" alt="banner das cores do practice, azul, amarelo e laranja">
+    </header>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            banner:'img/banner-practice.png'
+        }
+    }
+}
+</script>
