de24bf3737b7c5e178e64f6a946619cdc31b2480
Raphael Borges <rah-borges@hotmail.com>
2020-10-26 09:56:42 -0300

    (feat): nova home page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.base')
+@section('content')
+<section class="my-5">
+
+    <section>
+        <h1></h1>
+        <p>O Weebfeedback é a platafroma oficial do projeto Practice para que os nosso clientes possam solicitar e acompanhar serviços, alé de poder dar sugestões de melhorias e etc.</p>
+        <P>Buscando criar soluções tecnológicas para aprimorar o ambiente acadêmico de ensino e aprendizagem.</P>
+    </section>
+    
+    <services></services>
+    
+    <feedbacks></feedbacks>
+    
+</section>
+@endsection
+
