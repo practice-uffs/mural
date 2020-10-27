ba369eeb1ac162df79c6412bcbc4e01435c5fb2a
Raphael Borges <rah-borges@hotmail.com>
2020-10-27 15:24:44 -0300

    (feat):criado controller do feedback #125

---
+++ app/Http/Controllers/FeedbackController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FeedbackController extends Controller
+{
+    function index(){
+        return view('pages/itens');
+    }
+}
