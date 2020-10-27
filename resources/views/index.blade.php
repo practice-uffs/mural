24187876ab3c185ed8f799f404ecd7947f40bac9
Raphael Borges <rah-borges@hotmail.com>
2020-10-27 09:44:04 -0300

    (style):melhorado txt de apresentação WebFeedback

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,13 @@
 <section class="my-5">
 
     <section class="container">
-        <h1></h1>
-        <p>O Weebfeedback é a platafroma oficial do projeto Practice para que os nosso clientes possam solicitar e acompanhar serviços, alé de poder dar sugestões de melhorias e etc.</p>
-        <P>Buscando criar soluções tecnológicas para aprimorar o ambiente acadêmico de ensino e aprendizagem.</P>
+        <h1 class="text-center m-3">Weeb Feedback</h1>
+        <h2 class="text-center m-2">Uma ponte entre  Voccê e a construção de uma Universidade Melhor!</h2>
+        <p>O Weeb feedback é a platafroma oficial do projeto <a href="https://practice.uffs.cc/"><strong>Practice</strong></a> para que os nosso clientes possam solicitar e acompanhar serviços, 
+            além de poder dar sugestões de melhorias podendo assim, fazer parte do crescimento do projeto e fazer parte do crescimento e inovação da <a href="https://www.uffs.edu.br/"><strong>UFFS</strong></a>, crescimento e inovação que refletem em toda sua comunidade interna e externa.</p>
+        <P> A missão do projeto <a href="https://practice.uffs.cc/"><strong>Practice</strong></a> é empoderar tecnologicamente pessoas para que a tecnologia seja ajuda, não barreira. Focando na habilidade pedagógica e tecnológica tanto de servidores quanto estudantes. 
+            Tudo isso através de soluções tecnológicas para aprimorar o ambiente acadêmico de ensino e aprendizagem, contando sempre com a comunidade da <a href="https://www.uffs.edu.br/"><strong>UFFS</strong></a>.
+        </P>
     </section>
     
     <services></services>
