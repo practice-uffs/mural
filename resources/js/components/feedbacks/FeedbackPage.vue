5168605120268f83d9e310bfb74b4190aaba1d73
Raphael Borges <rah-borges@hotmail.com>
2020-11-06 13:24:35 -0300

    (refactor):includo o modal do feedback #129

--- resources/js/components/feedbacks/FeedbackPage.vue
+++ resources/js/components/feedbacks/FeedbackPage.vue
@@ -2,9 +2,9 @@
     <section>
     <div class="row d-flex align-items-center">
         <div class="col-sm-12 col-md-8 text-right">
-            <h2>Tem algo para compartilhar?</h2>
+            <h2>Tem algo para compartilhar? </h2>
             <p>Caso você tenha alguma idéia, crítica ou sugestão para melhorar e educação na UFFS? Compartile conosco (e com toda a comunidade)</p>
-            <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#exampleModal">Escrever</button>
+            <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#FeedbackModal">Escrever</button>
         </div>
         <div class="col-sm-12 col-md-4">
             <img class="img-feedback" :src="img" alt="">
@@ -12,21 +12,26 @@
     </div>
     <hr>
     <FeedbackList />
+    <FeedbackModal :user="user" />
     </section>
 </template>
 
 <script>
 import FeedbackList from './FeedbackList';
+import FeedbackModal from './FeedbackModal';
+
 export default {
     name:"feedbacks",
     components:{
         FeedbackList,
+        FeedbackModal,
     },
+    props:['user'],
     data(){
         return {
             img:'/img/undraw.co/Collaboration.png',
         }
-    }
+    },
 }
 </script>
 
