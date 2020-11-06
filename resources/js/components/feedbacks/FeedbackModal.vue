6396e10cd1ae291e302d467d93ca041afd62ee32
Raphael Borges <rah-borges@hotmail.com>
2020-11-06 13:23:27 -0300

    (feat):criado modal para add feedbacks #129

---
+++ resources/js/components/feedbacks/FeedbackModal.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- Modal -->
+<div class="modal fade" id="FeedbackModal" tabindex="-1" role="dialog" aria-labelledby="FeedbackModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="FeedbackModalLabel">Feedback</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <FeedbackForm :user="user"/>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+const FEEDBACK = 1;
+import FeedbackForm from './FeedbackForm';
+
+export default {
+    name:'FeedbackModal',
+    props:['feedback','user'],
+    components:{
+      FeedbackForm,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
