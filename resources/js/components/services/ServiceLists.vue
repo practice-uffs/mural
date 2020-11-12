387308294148020e1065c528124f45a4bf14daf7
Raphael Borges <rah-borges@hotmail.com>
2020-11-12 20:42:38 -0300

    (refactor):chamada dos serviços do practice e add videos #132 #133 #134 #135 #136

--- resources/js/components/services/ServiceLists.vue
+++ resources/js/components/services/ServiceLists.vue
@@ -6,13 +6,34 @@
                 v-for="service in services" :key="service.id" 
                 v-bind:service="service"/>
         <h3>Audio</h3>
-        <Service class="justify-content-center"
-                v-for="service in audios" :key="service.id" 
-                v-bind:service="service"/>
+        <div class="row">
+            <Service class="justify-content-center
+                            col-sm-12 col-md-6 "
+                    v-for="service in audios" :key="service.id" 
+                    v-bind:service="service"/>
+        </div>
         <h3>Vídeos</h3>
+        <div class="row">
+            <Service class="justify-content-center
+                            col-sm-12 col-md-6 "
+                    v-for="service in videos" :key="service.id" 
+                    v-bind:service="service"/>
+        </div>
         <h3>Estúdio</h3>
+            <Service class="justify-content-center
+                            col-sm-12 col-md-6 "
+                v-for="service in txts_imgs" :key="service.id" 
+                v-bind:service="service"/>
         <h3>Texto e Imagem</h3>
+            <Service class="justify-content-center
+                            col-sm-12 col-md-6 "
+                v-for="service in estudio" :key="service.id" 
+                v-bind:service="service"/>
         <h3>Evento</h3>
+            <Service class="justify-content-center
+                            col-sm-12 col-md-6 "
+                v-for="service in eventos" :key="service.id" 
+                v-bind:service="service"/>
     </div>
   </div>
 </template>
@@ -21,7 +42,7 @@
 import moment from 'moment'
 import MyServices from './MyServices';
 import Service from './Service';
-import {AUDIOS} from './json/services.json';
+import {AUDIOS,VIDEOS} from './json/services.json';
 
 Vue.filter('formatDate', function(value) {
     if (value) {
@@ -39,7 +60,11 @@ export default {
     data(){
         return {
             services:[],
-            audios: AUDIOS
+            audios: AUDIOS,
+            videos: VIDEOS,
+            txts_imgs: [],
+            estudio: [],
+            eventos: [],
         }
     },
     methods:{
