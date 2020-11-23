392f7b362fc8d0ff0c37a57e901d5a29ddd20d34
Raphael Borges <rah-borges@hotmail.com>
2020-11-23 16:05:35 -0300

    (feat):criad o seed das especificações de serviços #159

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             LocationSeeder::class,
+            SpecificationSeeder::class,
         ]);
     }
 }
