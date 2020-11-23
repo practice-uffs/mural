723d03bb4f9969daccf9fd8489277f9069732528
Raphael Borges <rah-borges@hotmail.com>
2020-11-23 14:12:48 -0300

    (fix):edit table items corretamente #157

--- database/migrations/2020_07_06_185039_create_items_table.php
+++ database/migrations/2020_07_06_185039_create_items_table.php
@@ -26,7 +26,6 @@ class CreateItemsTable extends Migration
             $table->integer('type')->default(1);
             $table->string('title', 200)->default('');
             $table->text('description')->nullable();
-            $table->text('github_issue_link')->nullable();
             $table->boolean('hidden')->default(false);
             $table->timestamps();
 
