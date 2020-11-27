3377b7fc6499d37b22900f71d3ee246e887ab86d
Raphael Borges <rah-borges@hotmail.com>
2020-11-27 10:45:43 -0300

    (refactor):ajust novas features dos serviços #160

--- app/Http/Controllers/API/ServiceController.php
+++ app/Http/Controllers/API/ServiceController.php
@@ -57,19 +57,14 @@ class ServiceController extends Controller
             'user_id' => 'required',
             'location_id' => 'required',
             'category_id' => 'required',
+            'specification_id' => 'required',
             'title' => 'required',
             'description' => 'required'
         ]);
         
-        $data['hidden'] = true;
+        $data['hidden'] = false;
         $data['type'] = Item::TYPE_SERVICE;
         $data['status'] = Item::STATUS_ACTIVE;
-
-        $specification = Specification::create([
-            'content' => json_encode($request -> specification)
-        ]);
-
-        $data['specification_id'] = $specification -> id;
 
         $item = Item::create($data);
 
@@ -107,6 +102,8 @@ class ServiceController extends Controller
             'location_id' => 'required',
             'category_id' => 'required',
             'title' => 'required',
+            'specification_id' => 'required',
+            'github_issue_link' => 'required',
             'description' => 'required'
         ]);
 
@@ -115,13 +112,8 @@ class ServiceController extends Controller
         $data['status'] = Item::STATUS_ACTIVE;
 
         $service = Item::find($id);
-        $specification = Specification::find($service->specification_id);
 
-        try {
-            $specification->update([
-                'content' => json_encode($request -> specification)
-            ]);
-            
+        try {           
             $service->update($data);
 
             return response(
