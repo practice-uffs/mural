bf3ed85072be7bec9fa3fa583a5557ebb6ea8b73
Raphael Borges <rah-borges@hotmail.com>
2020-11-30 16:35:18 -0300

    (feat):validações para geração do token #170

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers\API;
 
+use App\User;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    const ADMIN_USERS = [
+        'jean.hilger',
+        'junior.ramisch',
+        'raphael.santos'
+    ];
         /**
      * Get a JWT via given credentials.
      *
@@ -16,10 +23,30 @@ class AuthController extends Controller
     {
         $credentials = $request->only(['username','password']);
 
+        //Gera o token e realiza o Login do usuário
         if (! $token = auth('api')->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
 
+            // Confere se esse usuário existe na base idUFFS
+            $credentials_uffs = [
+                'user'     => $request->input('username'),
+                'password' => $request->input('password'),
+            ];
+            $auth = new \CCUFFS\Auth\AuthIdUFFS();
+            $userData = $auth->login($credentials_uffs);
+            
+            if (!$userData) {
+                return response()->json(['error' => 'usuário ou senha incorreta'], 401);
+            }
+
+            // Caso usuário exista na base idUFFS, ele criará o usuário no webfeedback
+            $userData->password = bcrypt($credentials['password']);
+            $user = $this->getOrCreateUser($userData);
+            // e tentará gerar o token e fazer login novamente
+            if (! $token = auth('api')->attempt($credentials)) {
+                return response()->json(['error' => 'não autorizado'], 401);
+            }
+            return $this->respondWithToken($token);
+        }
         return $this->respondWithToken($token);
     }
         /**
@@ -38,4 +65,31 @@ class AuthController extends Controller
         ]);
     }
 
+    private function getOrCreateUser($data)
+    {
+        $user = User::where('uid', $data->uid)->first();
+        $data = [
+            'username' => $data->username,
+            'password' => $data->password,
+            'email' => $data->email,
+            'uid' => $data->uid,
+            'name' => $data->name,
+            'cpf' => $data->cpf
+        ];
+
+        if (in_array($data['username'], SELF::ADMIN_USERS)) {
+            $data['type'] = User::ADMIN;
+        
+        } else {
+            $data['type'] = User::NORMAL;
+        }
+
+        if ($user) {
+            $user->update($data);
+        } else {
+            $user = User::create($data);
+        }
+
+        return $user;
+    }
 }
