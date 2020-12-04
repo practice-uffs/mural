0d9ca4ffc4d7646554dc231443ee007990267b1d
Raphael Borges <rah-borges@hotmail.com>
2020-12-04 19:12:53 -0300

    (refact):Página dos itens e seus comentários #184

--- resources/js/components/items/CommentList.vue
+++ resources/js/components/items/CommentList.vue
@@ -1,187 +1,113 @@
 <template>
-    <div id="timeline" class="timeline">
-        <ul class="timeline__list">
-            <li
-                v-for="comment in comments"
-                v-bind="comment"
-                :key="comment.id"
-                class="timeline__item"
-            >
-                <div class="timeline__icon">
-                </div>
-
-                <div class="timeline__content">
-                    <div class="timeline__title">
-                        {{ comment.user | capitalize }}
-                        <span>
-                            {{ comment.date | formatDate }}
-                        </span>
-                    </div>
-
-
-                    <div class="timeline__text">
-                        {{ comment.text }}
-                    </div>
-
-                    <div class="timeline__reactions">
-                        <reaction-list
-                            :user-id="userId"
-                            :item-id="comment.id"
-                        >
-                        </reaction-list>
-                    </div>
-                </div>
-            </li>
-        </ul>
-
-        <div
-            class="card timeline__card"
-            v-if="userId"
+    <div>
+        <h6>Comentários ({{ comments.length }})</h6>
+        <div class="form-group p-3 card" v-bind="comment"
+            v-for="comment in comments" :key="comment.id">
+            <label><strong>{{ comment.user | capitalize }}</strong></label>
+            <p>{{comment.text}}</p>
+            <small class="text-right" >{{comment.date | prettyDate}}</small>
+        </div>
+        <!-- <reaction-list
+            :user-id="userId"
+            :item-id="comment.id"
         >
-            <div class="card-content">
-                <div class="card-title">
-                    Adicionar Comentário
-                </div>
-                <div class="input-field">
-                    <textarea
-                        class="materialize-textarea"
-                        id="userComment"
-                        name="userComment"
-                        v-model="userComment"
-                        rows="8"
-                    ></textarea>
-                </div>
+        </reaction-list> -->
 
-                <div class="card-action">
-                    <in-place-loader ref="loader">
-                        <a
-                            class="btn btn--primary btn--gradient"
-                            @click="createComment"
-                        >
-                            Comentar
-                        </a>
-                    </in-place-loader>
-                </div>
-            </div>
+        <form @submit.prevent="createComment" class="card p-4">
+            <h6> Adicionar um Comentários</h6>
+            <textarea id="userComment"  name="userComment"
+                class="form-control" v-model="userComment"
+                rows="4">
+            </textarea>
+            <button class="btn btn-warning mt-3"
+                    type="submit"
+            >Comentar</button>
+        </form>
 
         </div>
     </div>
 </template>
 
 <script>
+import Swal from 'sweetalert2';
 export default {
-    data: function() {
+    props: ['user','itemId'],
+    data() {
         return {
             comments: [],
-
             userComment: '',
         }
     },
-
-    props: {
-        'userId': {
-            default: null,
-        },
-
-        'itemId': [String, Number],
-    },
-
     methods: {
         async fetchComments() {
-            /**
-             * Fetch all comments for a given item.
-             */
-
-            let { data } = await window.axios.get(`/api/items/${this.itemId}/comments`);
-
+            let { data } = await window.axios.get(`/api/service/${this.itemId}/comments`);
             this.comments = data.data;
         },
 
         async createComment() {
-            /**
-             * Creates a new comment.
-             */
-
-            this.$refs.loader.start();
-            if (this.userComment == '') return;
-
-            let comment = await window.axios.post(`/api/items/${this.itemId}/comments`, {
-                'user_id': this.userId,
-                'text': this.userComment
-            });
-
-            this.comments.push(comment.data);
-            this.$refs.loader.finish();
-
-            this.userComment = '';
-
-            if (!this.hasComment()) {
-                document.querySelector('#timeline .timeline__card').style.marginTop = '0';
-
-            } else {
-                document.querySelector('#timeline .timeline__item:last-child').style.paddingBottom = '0';
+            let data = {
+            "user_id": this.user.id,
+            "user": this.user.name,
+            "text": this.userComment,
+            };
+            console.log(data);
+            try {
+                let response = await window.axios.post(`/api/service/${this.itemId}/comments`,data);
+                this.handleSuccess(response);
             }
-            
-            this.userComment = '';
+            catch(err) {
+                this.handleError(err);
+            }
         },
 
-        hasComment() {
-            /**
-             * Returns whether there exists any comment on comment list.
-             */
+        handleError(err){
+            let data = err.response.data;
+            console.log(data);
+            const Toast = Swal.mixin({
+                    toast: true,
+                    position: 'center',
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.addEventListener('mouseenter', Swal.stopTimer)
+                        toast.addEventListener('mouseleave', Swal.resumeTimer)
+                    }
+                })
 
-            return (typeof this.comments !== 'undefined' && this.comments.length > 0);
+                Toast.fire({
+                    icon: 'error',
+                title: 'Falha no envio do Feedback, por favor tente mais tarde!!'
+                })
         },
 
-        adjustLastItemPosition() {
-            /**
-             * If none comment exists yet, add a spacing.
-             */
-
-
-            if (this.hasComment()) {
-                let lastItem = document.querySelector('#timeline .timeline__item:last-child');
-                let padding = lastItem.querySelector('.timeline__content').offsetHeight;
-                if (this.userId) {
-                    lastItem.style.paddingBottom = padding * 1.3 + 'px';
-
-                } else {
-                    lastItem.style.marginBottom = padding + 'px';
+        handleSuccess(response){
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'center',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
                 }
+                })
 
-            } else {
-                document.querySelector('#timeline .timeline__card').style.marginTop = '4rem';
-            }
+                Toast.fire({
+                    icon: 'success',
+                title: 'Feedback Enviado com sucesso!!'
+                }).then(function(){
+                    location.reload();
+                })
+            this.resetData();
+            
         },
-    },
-
-    filters: {
-        capitalize: function (value) {
-            return value.replace(/\w\S*/g, function (txt) {
-                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-            });
-        },
-
-        formatDate: function (date) {
-            /**
-             * Returns a date on the format dd/mm/YYYY.
-             */
-            let fullDate = new Date(date);
-
-            let day = fullDate.getDate();
-            let month = fullDate.getMonth() + 1;
-            let year = fullDate.getFullYear();
-
-            return `${day}/${month.toString().padStart(2, '0')}/${year}`;
-        }
     },
 
     created() {
         this.fetchComments();
     },
 
-    updated() {
-        this.adjustLastItemPosition();
-    }
 }
 </script>
---
+++ resources/js/components/items/ItemPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="col-8">
+      <h4>{{item.title}}</h4>
+      <p v-if="item.github_issue_link"><a :href="item.github_issue_link" target="_blank"><small>Acompanhe essa solicitação no GitHub</small></a></p>
+      <p>{{item.description}}</p>
+      <p class="text-right"> <small> última atualização {{item.updated_at | prettyDate}}</small></p>
+    <hr>
+    <comment-list :user="user" :item-id="item.id"></comment-list>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Comments',
+    props:['user','item'],
+}
+</script>
+
+<style>
+
+</style>
