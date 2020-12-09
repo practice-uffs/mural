6c0edaffa73e85139f0c8ffc36630b170f4df836
Raphael Borges <rah-borges@hotmail.com>
2020-12-09 14:13:09 -0300

    style:issue link, catego, espefic, local #194

--- resources/js/components/admin/Services.vue
+++ resources/js/components/admin/Services.vue
@@ -3,7 +3,7 @@
                d-flex justify-content-around align-items-center">
         <a :href="service.github_issue_link">
             <img class="github-icon"
-                 :class="{'inactive':service.github_issue_link !== ''}"
+                 :class="{'inactive':!service.github_issue_link}"
                  :src="'/img/GitHub-Mark-64px.png'"
                  alt="link para a isssue no github" title="link para a isssue no github"
             >
@@ -11,10 +11,11 @@
         <div class="'col-sm-11 col-md-6 text-left">
             <p><strong>{{service.title}} </strong><small>- atualizadada {{service.updated_at | prettyDate}}</small> </p>
             <p>{{service.description.substring(0,150)+"..." }}</p>
+            <p><small>Categoria: {{service.specification_id}}, Localização: {{service.location_id}}</small></p>
         </div>
         <div>
             <p><strong>Usuario</strong></p>
-            <p>{{service.user.username}}</p>
+            <p>{{service.user}}</p>
         </div>
         <div>
             <p><strong>Categoria</strong></p>
@@ -32,7 +33,7 @@
            class="row col-sm-12 col-md-1 
                   justify-content-center ">
             <span class="material-icons">insert_comment</span>
-            <p>Acompanhar</p>
+            <p><small>Acompanhar</small></p>
         </a>
     </div>
 </template>
