75dad78bafb0c276d7cb8ea078212154df72ff42
Raphael Borges <rah-borges@hotmail.com>
2020-12-09 14:14:37 -0300

    (refactor): retorno serviço #194

--- app/Http/Resources/ServiceResource.php
+++ app/Http/Resources/ServiceResource.php
@@ -4,8 +4,10 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-use App\Specification;
 use App\User;
+use App\Location;
+use App\Category;
+use App\Specification;
 
 class ServiceResource extends JsonResource
 {
@@ -21,14 +23,15 @@ class ServiceResource extends JsonResource
             'id' => $this->id,
             'user_id' => $this->user_id,
             'user' => User::find($this->user_id)->username,
-            'location_id' => $this->location_id,
-            'category_id' => $this->category_id,
-            'specification_id' => $this->specification_id,
+            'location_id' => Location::find($this->location_id)->name,
+            'category_id' => Category::find($this->category_id)->name,
+            'specification_id' => Specification::find($this->specification_id)->title,
             'status' => $this->status,
             'title' => $this->title,
             'description' => $this->description,
             'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at
+            'updated_at' => $this->updated_at,
+            'github_issue_link' => $this->github_issue_link,
         ];
     }
 }
