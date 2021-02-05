1bc8dbe6e280a8ada5e8cb45a8a1a7ae7389d96b
Raphael Borges <rah-borges@hotmail.com>
2021-02-05 16:52:56 -0300

    refact(admin):add admin link #210 #223

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,10 +10,15 @@
 
       <nav id="navbar" class="navbar">
         <ul>
+          @if(Auth::check())
           <li><a class="nav-link scrollto @if (request()->routeIs('index')) active @endif" href="{{ route('index') }}">Inicial</a></li>
           <li><a class="nav-link scrollto @if (request()->routeIs('feedbacks')) active @endif" href="{{ route('feedbacks') }}">Ideias</a></li>
           <li><a class="nav-link scrollto @if (request()->routeIs('services')) active @endif" href="{{ route('services') }}">Acompanhar serviços</a></li>
           <li><a class="nav-link scrollto @if (request()->routeIs('services')) active @endif" href="{{ route('services') }}">Solicitar serviço</a></li>
+            @if(Auth()->user()->type == "admin")
+              <li><a class="nav-link scrollto @if (request()->routeIs('admin')) active @endif" href="{{ route('admin') }}">Admin</a></li>
+            @endif
+          @endif
           @if (Auth::check())
             <li class="dropdown mx-4">
                 <a href="#">
