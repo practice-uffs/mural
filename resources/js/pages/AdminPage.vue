8f94dba562d88eabae41f9dbae268889b4fede67
Raphael Borges <rah-borges@hotmail.com>
2021-02-10 15:45:21 -0300

    refact(admin):componets

--- resources/js/pages/AdminPage.vue
+++ resources/js/pages/AdminPage.vue
@@ -1,12 +1,46 @@
 <template>
-<div>
-    <h1>Gerenciamento de Serviços</h1>
+<section>
+    <div class="row d-flex align-items-center">
+        <div class="col-sm-12 col-md-7 text-right">
+            <h1>Gerenciamento de Serviços Solicitados</h1>
+            <p>Acompanhe aqui os serviços solicitados pelo clientes do PRACTICE</p>
+        </div>
+        <div class="col-sm-12 col-md-5">
+            <img class="img-service" :src="img" alt="">
+        </div>
+    </div>
+    <hr>
 
-    <h2>Serviços</h2>
-    <Services
-        v-for="service in services" :key="service.id" 
-        v-bind:service="service"/>
-</div>
+    <div v-if="!!aguardando.length">
+        <h2 clas="mt-5">Serviços aguardando aprovação</h2>
+        <Services
+            v-for="service in aguardando" :key="service.id" 
+            v-bind:service="service"/>
+    </div>
+    <div v-else>
+        <h3 class="mt-5">Não há serviços solicitados em aprovação no momento</h3>
+    </div>
+
+    <div v-if="!!progredindo.length">
+        <h2 clas="mt-5">Serviços em progresso</h2>
+        <Services
+            v-for="service in progredindo" :key="service.id" 
+            v-bind:service="service"/>
+    </div>
+    <div v-else>
+        <h3 class="mt-5">Não há serviços solicitados em progresso no momento</h3>
+    </div>
+
+    <div v-if="!!concluido.length">
+        <h2 clas="mt-5">Serviços concluídos</h2>
+        <Services
+            v-for="service in concluido" :key="service.id" 
+            v-bind:service="service"/>
+    </div>
+    <div v-else>
+        <h3 class="mt-5">Não há serviços solicitados e concluídos no momento</h3>
+    </div>
+</section>
 </template>
 
 <script>
@@ -21,6 +55,10 @@ export default {
     data(){
         return {
             services:[],
+            aguardando: [],
+            progredindo: [],
+            concluido: [],
+            img:'/img/undraw.co/admin.png',
         }
     },
     mounted(){
@@ -39,6 +77,15 @@ export default {
 
             this.services = data.data.reverse();
 
+            this.aguardando = this.services.filter((servico) => {
+                return servico.status === 1;
+            })
+            this.progredindo = this.services.filter((servico) => {
+                return servico.status === 2;
+            })
+            this.concluido = this.services.filter((servico) => {
+                return servico.status === 3;
+            })
         },
     },
     created() {
@@ -47,6 +94,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.img-service{
+    width: 400px;
+}
 </style>
