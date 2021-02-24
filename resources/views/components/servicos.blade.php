0db124b35056c2f0413622559fb1c9bed8f12c80
Raphael Borges <rah-borges@hotmail.com>
2021-02-24 14:52:53 -0300

    style(home):componentizado #257

---
+++ resources/views/components/servicos.blade.php
@@ -0,0 +1,87 @@
+<!-- ======= Services Section ======= -->
+<section id="services" class="services">
+
+    <div class="container">
+
+      <header class="section-header">
+        <h2>Serviços</h2>
+        <p>Como podemos ajudar</p>
+      </header>
+
+      <div class="row gy-4">
+
+        <div class="col-lg-4 col-md-6" data-aos-delay="200">
+          <div class="service-box red">
+            <i class="ri-discuss-line icon"></i>
+            <h3>Áudio</h3>
+            <p>Faixas de áudio com objetivo de anunciar e/ou divulgar algo, fazr uma chamada ou
+               áudios com a possibilidade de trilha e efeitos sonoros mais elaborados e composição de diferentes áudios, 
+               como entrevistas e/ou leitura de livros.
+            </p>
+            <a href="/servicos/solicitar" class="read-more"><span>Saiba mais</span> <i class="bi bi-arrow-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6" data-aos-delay="300">
+          <div class="service-box orange">
+            <i class="ri-discuss-line icon"></i>
+            <h3>Vídeo</h3>
+            <p>Há diversos serviços de vídeo podendo ser: 
+               Vídeo Aula Simples com apenas o interlocutor em plano principal,
+               Hand Drawn uma nimação ilustrando a explicação da fala do narrador ou o próprio narrador escrevendo em tempo real.
+               Vídeo Tutorial explicativo ensinando passo a passo da utilização de uma ferramenta e/ou software, 
+               entre outros.
+              </p>
+            <a href="/servicos/solicitar" class="read-more"><span>Saiba mais</span> <i class="bi bi-arrow-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6" data-aos-delay="400">
+          <div class="service-box yellow">
+            <i class="ri-discuss-line icon"></i>
+            <h3>Texto</h3>
+            <p>
+              Visando a tramissão de conteúdo os serviços de texto incluem a produção de: cartilhas, manuais, panfletos e cartazes.
+            </p>
+            <a href="/servicos/solicitar" class="read-more"><span>Saiba mais</span> <i class="bi bi-arrow-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6" data-aos-delay="500">
+          <div class="service-box green">
+            <i class="ri-discuss-line icon"></i>
+            <h3>Imagem</h3>
+            <p>
+              Precisnado de uma identidade visual, podemos providenciar com nossos pacotes criando logotipo, símbolo, paletas de cores, padronagem outros itens a mais.
+            </p>
+            <a href="/servicos/solicitar" class="read-more"><span>Saiba mais</span> <i class="bi bi-arrow-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6" data-aos-delay="600">
+          <div class="service-box blue">
+            <i class="ri-discuss-line icon"></i>
+            <h3>Mídia</h3>
+            <p>
+              Precisando de auxilio para a realização de eventos vituais com lives e interação do público, podemos fornecer as ferramentas e suporte para que seja isso seja possível.
+            </p>
+            <a href="/servicos/solicitar" class="read-more"><span>Saiba mais</span> <i class="bi bi-arrow-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6" data-aos-delay="700">
+          <div class="service-box purple">
+            <i class="ri-discuss-line icon"></i>
+            <h3>Estúdio</h3>
+            <p>
+              Temos um estúdio equipado com uma infraestrutura que possa te ajudar a realização das suas produções de vídeos.
+            </p>
+            <a href="/servicos/solicitar" class="read-more"><span>Saiba mais</span> <i class="bi bi-arrow-right"></i></a>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </section><!-- End Services Section -->
