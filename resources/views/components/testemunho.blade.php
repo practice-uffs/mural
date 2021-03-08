b95a478ae38d8ecfaf56c329865f5561e4d33799
Raphael Borges <rah-borges@hotmail.com>
2021-03-08 20:23:36 -0300

    style(home):add CL comment #252

--- resources/views/components/testemunho.blade.php
+++ resources/views/components/testemunho.blade.php
@@ -1,100 +1,85 @@
  <!-- ======= Testimonials Section ======= -->
  <section id="testimonials" class="testimonials">
 
-    <div class="container">
+  <div class="container">
 
-      <header class="section-header">
-        <h2>Testemunhos</h2>
-        <p>O que dizem sobre nós e nosso trabalho</p>
-      </header>
+    <header class="section-header">
+      <h2>Testemunhos</h2>
+      <p>O que dizem sobre nós e nosso trabalho</p>
+    </header>
 
-      <div class="testimonials-slider swiper-container" data-aos="fade-up" data-aos-delay="200">
-        <div class="swiper-wrapper">
+    <div class="testimonials-slider swiper-container" data-aos="fade-up" data-aos-delay="200">
+      <div class="swiper-wrapper">
 
-          <div class="swiper-slide">
-            <div class="testimonial-item">
-              <div class="stars">
-                <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
-              </div>
-              <p>
-                Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
-              </p>
-              <div class="profile mt-auto">
-                <img src="{{ asset('assets/img/testimonials/testimonials-1.jpg') }}" class="testimonial-img" alt="">
-                <h3>Saul Goodman</h3>
-                <h4>Ceo &amp; Founder</h4>
-              </div>
+        <div class="swiper-slide">
+          <div class="testimonial-item">
+            <p>
+              "Gostei muito do atendimento, todos são muito gentis e educados. Algumas vezes o palestrante está nervoso antes de iniciar a atividade e ter um apoio técnico eficiente ajuda muito nesse quesito. Além disso, nós não temos conhecimento para essa função, contar com o apoio do Practice foi muito importante."
+            </p>
+            <div class="profile mt-auto">
+              
+              <h3>Cliente PRACTICE</h3>
+              <h4>Depoimento coletado através da pesquisa de satisfação de serviços prestados</h4>
             </div>
-          </div><!-- End testimonial item -->
+          </div>
+        </div><!-- End testimonial item -->
 
-          <div class="swiper-slide">
-            <div class="testimonial-item">
-              <div class="stars">
-                <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
-              </div>
-              <p>
-                Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
-              </p>
-              <div class="profile mt-auto">
-                <img src="{{ asset('assets/img/testimonials/testimonials-2.jpg') }}" class="testimonial-img" alt="">
-                <h3>Sara Wilsson</h3>
-                <h4>Designer</h4>
-              </div>
+        <div class="swiper-slide">
+          <div class="testimonial-item">
+            <p>
+              "Um dos serviços do PRACTICE que solicitei foi a criação de um vídeo que explicasse o novo regulamento e ACCs do curso de Ciência da Computação. Eu esperava um video bom, mas o resultado foi fantástico. Um material de difícil leitura e animação foi animado e narrado em video com muita propriedade. O roteiro do video me deixou muito, mas muito impressionado mesmo. Aliado a isso, a narração e carisma do vídeo são incríveis."
+            </p>
+            <div class="profile mt-auto">
+              
+              <h3>Cliente PRACTICE</h3>
+              <h4>Depoimento coletado através da pesquisa de satisfação de serviços prestados</h4>
             </div>
-          </div><!-- End testimonial item -->
+          </div>
+        </div><!-- End testimonial item -->
 
-          <div class="swiper-slide">
-            <div class="testimonial-item">
-              <div class="stars">
-                <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
-              </div>
-              <p>
-                Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
-              </p>
-              <div class="profile mt-auto">
-                <img src="{{ asset('assets/img/testimonials/testimonials-3.jpg') }}" class="testimonial-img" alt="">
-                <h3>Jena Karlis</h3>
-                <h4>Store Owner</h4>
-              </div>
+        <div class="swiper-slide">
+          <div class="testimonial-item">
+            <p>
+              "Acho o Practice um Programa muito importante que veio valorizar nossas produções digitais, de extrema importância, ainda mais em tempo de grande uso destes recursos (pandemia)."
+            </p>
+            <div class="profile mt-auto">
+              
+              <h3>Cliente PRACTICE</h3>
+              <h4>Depoimento coletado através da pesquisa de satisfação de serviços prestados</h4>
             </div>
-          </div><!-- End testimonial item -->
+          </div>
+        </div><!-- End testimonial item -->
 
-          <div class="swiper-slide">
-            <div class="testimonial-item">
-              <div class="stars">
-                <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
-              </div>
-              <p>
-                Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
-              </p>
-              <div class="profile mt-auto">
-                <img src="{{ asset('assets/img/testimonials/testimonials-4.jpg') }}" class="testimonial-img" alt="">
-                <h3>Matt Brandon</h3>
-                <h4>Freelancer</h4>
-              </div>
+        <div class="swiper-slide">
+          <div class="testimonial-item">
+            <p>
+              "Parabéns. O resultado do card impressionou a equipe. Excelente trabalho."
+            </p>
+            <div class="profile mt-auto">
+              
+              <h3>Cliente PRACTICE</h3>
+              <h4>Depoimento coletado através da pesquisa de satisfação de serviços prestados</h4>
             </div>
-          </div><!-- End testimonial item -->
+          </div>
+        </div><!-- End testimonial item -->
 
-          <div class="swiper-slide">
-            <div class="testimonial-item">
-              <div class="stars">
-                <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
-              </div>
-              <p>
-                Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
-              </p>
-              <div class="profile mt-auto">
-                <img src="{{ asset('assets/img/testimonials/testimonials-5.jpg') }}" class="testimonial-img" alt="">
-                <h3>John Larson</h3>
-                <h4>Entrepreneur</h4>
-              </div>
+        <div class="swiper-slide">
+          <div class="testimonial-item">
+            <p>
+              "Eu posso dizer que o produto final superou todas as minhas expectativas de uma forma muito significativa. Sinto muito orgulho de fazer parte de uma universidade que tenha um serviço dessa qualidade. O vídeo em questão afetará positivamente a vida de centenas de acadêmicos de Ciência da Computação por muitos anos. Parabéns!"
+            </p>
+            <div class="profile mt-auto">
+              
+              <h3>Cliente PRACTICE</h3>
+              <h4>Depoimento coletado através da pesquisa de satisfação de serviços prestados</h4>
             </div>
-          </div><!-- End testimonial item -->
+          </div>
+        </div><!-- End testimonial item -->
 
-        </div>
-        <div class="swiper-pagination"></div>
       </div>
-
+      <div class="swiper-pagination"></div>
     </div>
 
-  </section><!-- End Testimonials Section -->
+  </div>
+
+</section><!-- End Testimonials Section -->
