e96125c914583d1a95609195ddb78abd20310ee4
Raphael Borges <rah-borges@hotmail.com>
2021-03-22 09:32:02 -0300

    refact(sweetalert):add sweetalert #272

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -54,6 +54,7 @@
         <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
         
         <!-- Vendor JS Files -->
+        <script src="{{ asset('vendor/sweetalert2/dist/sweetalert2.min.js') }}"></script>
         <script src="{{ asset('vendor/aos/aos.js') }}"></script>
         <script src="{{ asset('vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
         <script src="{{ asset('vendor/glightbox/js/glightbox.min.js') }}"></script>
