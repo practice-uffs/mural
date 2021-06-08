59562a22c0016da2ae32088d354b08fc4cb56c8a
alisson.luan2000@gmail.com <alisson.luan2000@gmail.com>
2021-06-08 07:54:32 -0300

    feat(add delivery date on admin page)

--- resources/js/components/admin/Services.vue
+++ resources/js/components/admin/Services.vue
@@ -1,61 +1,85 @@
 <template>
      <div class="p-1 col-sm-12 col-md-4">
+        <div class="my-service text-center" :class="service.status|status_class">
 
-        <div class="my-service text-center " :class="service.status|status_class">
+            <div class="w-100">
+                <div class="col-sm-12 p-3 text-start w-100">
+                    <p class="title mb-1"><strong>{{service.title}}</strong></p>
+                    <p>{{service.description.substring(0,150)}} <i v-if="service.description.length > 150">...</i></p>
+                    <p class="text-muted"><small>Solicitado por: {{service.user}}</small></p>
+                </div>
 
-            <div class="col-sm-12 text-start">
-                <p><strong>{{service.title}}</strong></p>
-                <p>{{service.description.substring(0,150)+"..." }}</p>
-                <p class="text-muted"><small>Solicitado por: {{service.user}}</small></p>
+                <div class="p-1 w-100">
+                    <div class="col-12 mb-2 d-flex justify-center align-items-stretch flex-wrap">
+                        <div class="d-flex flex-column justify-content-center item_box">
+                            <div class="p-2">
+                                <b>Id</b>
+                            </div>
+                            <div class="p-2">
+                                {{service.id}}
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column justify-content-center item_box">
+                            <div class="p-2">
+                                <b>Categoria</b>
+                            </div>
+                            <div class="p-2">
+                                {{service.specification_id}}
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column justify-content-center item_box">
+                            <div class="p-2">
+                                <b>Data</b>
+                            </div>
+                            <div class="p-2">
+                                {{service.created_at | formatDate}}
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column justify-content-center item_box" v-if="service.delivery_date">
+                            <div class="p-2">
+                                <b>Data de Entrega</b>
+                            </div>
+                            <div class="p-2">
+                                {{service.delivery_date | formatDate}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <table class="col-12 mb-2">
-                <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Categoria</th>
-                    <th>Data</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>{{service.id}}</td>
-                    <td>{{service.specification_id}}</td>
-                    <td>{{service.created_at | formatDate}}</td>
-                </tr>
-                </tbody>
-            </table>
-
-            <div class="col-12 mt-2 row">
-                <div class="col-sm-6">
-                    <div class="row">
-                        <a :href="service.github_issue_link" class="col-12">
-                            <img class="github-icon"
-                                :class="{'inactive':!service.github_issue_link}"
-                                :src="'/img/GitHub-Mark-64px.png'"
-                                alt="link para a isssue no github" title="link para a isssue no github"
-                            >
+            <div class="w-100">
+                <div class="d-flex flex-row justify-content-center w-100 p-2">
+                    <div class="col-sm-5">
+                        <a :href="'/servico/'+service.id+'/edit'">
+                        <span class="material-icons col-12">grading</span>
+                        Editar</a>
+                    </div>
+                    <div class="col-sm-5">
+                        <a :href="'/servico/'+service.id">
+                            <span class="material-icons">insert_comment</span>
+                            <p>Acompanhar</p>
                         </a>
-                        <a :href="'/servico/'+service.id+'/edit'"
-                            class="p-2"
-                        >Editar</a>
-                    </div>   
+                    </div>
                 </div>
-                <div class="col-sm-6">
-                    <a :href="'/servico/'+service.id">
-                        <span class="material-icons">insert_comment</span>
-                        <p>Acompanhar</p>
+
+                <div class="col-sm-2">
+
+                </div>
+
+                <div class="col-12 text-end text-muted p-3 d-flex justify-content-between align-items-center">
+                    <a :href="service.github_issue_link">
+                        <img class="github-icon"
+                            :class="{'inactive':!service.github_issue_link}"
+                            :src="'/img/GitHub-Mark-64px.png'"
+                            alt="link para a isssue no github" title="link para a isssue no github"
+                        >
                     </a>
+
+                    <small><i class="bi bi-alarm"></i> última atualização {{service.updated_at | prettyDate}}</small>
                 </div>
             </div>
-
-            <div class="col-12 text-end text-muted "> 
-                <small><i class="bi bi-alarm"></i> última atualização {{service.updated_at | prettyDate}}</small>
-            </div>
-
         </div>
-
-   </div>
+    </div>
 </template>
 
 <script>
@@ -71,15 +95,16 @@ export default {
     background-color: #f0f0f0;
     border-left: 5px solid;
     border-radius: 8px;
-    padding: 30px;
+    padding: 0px, 30px;
     height: 100%;
 
     display: flex;
+    flex-direction: column;
     flex-wrap: wrap;
-    flex-direction: row;
-    align-items: center;
+    justify-content: space-between;
     align-content: center;
-    justify-content: space-around;
+    align-items: center;
+    word-wrap: break-word;
 }
 .my-service:hover{
     background-color: #ededed;
@@ -120,9 +145,17 @@ a, a:hover, a:visited, a:active{
     border-color: #ff0000;
     transition: 0.3s;
 }
-
+.item_box{
+    flex: 1;
+    min-width: 30%;
+    min-height: 100px;
+    border: 1px solid rgba(68, 68, 68, 0.25);
+    border-radius: 8px;
+    margin: 5px;
+    word-wrap: break-word;
+}
 .github-icon{
-    width: 25px;
+    width: 30px;
 }
 .inactive{
     filter: opacity(0.5);
