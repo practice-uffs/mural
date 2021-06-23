d5ae8d93b54c33adb1ed8ca851f2fd06d9b66df4
Guilherme Graeff <guilherme.rafael.graeff@gmail.com>
2021-06-23 13:04:27 -0300

    fix: Aparecer nome do último a comentar no serviço

--- resources/js/components/items/CommentList.vue
+++ resources/js/components/items/CommentList.vue
@@ -1,35 +1,37 @@
 <template>
     <div>
-        <h6>Comentários ({{ comments.length }})</h6>
-        <div v-if="comments">
-        <div class="form-group p-3 card" v-bind="comment"
-            v-for="comment in comments" :key="comment.id">
-
-            <div class="d-flex justify-content-between">
-                <label><strong>{{ comment.user | capitalize }}</strong></label>
-                <div class="dropdown" v-if="user.id == comment.user_id">
-                    <button class="btn btn-sm" type="button" :id="'dropdownMenuButton-'+comment.id" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="ri-more-2-fill"></i>
-                    </button>
-                    <ul class="dropdown-menu" :aria-labelledby="'dropdownMenuButton-'+comment.id">
-                        <a class="dropdown-item" @click="deleteComment(comment.id)">Excluir</a>
-                    </ul>
-                </div>
+        <div class="d-flex justify-content-between">
+            <div>
+                <h6>Comentários ({{ comments.length }})</h6>
             </div>
-            <p>{{comment.text}}</p>
 
-            <label><strong>{{ comment.user | capitalize }}</strong></label>
-            <p style="white-space:pre-wrap">{{comment.text}}</p>
-
-            <small class="text-end" >{{comment.date | prettyDate}}</small>
+            <div v-if="comments[comments.length -1]">
+                <small>última atualização {{comments[comments.length -1].date | prettyDate}} realizada por {{comments[comments.length -1].user | capitalize}}</small>
+            </div>
+        </div>
+        <div v-if="comments">
+            <div class="form-group p-3 card" v-bind="comment"
+                v-for="comment in comments" :key="comment.id">
+                <div class="d-flex justify-content-between">
+                    <label><strong>{{ comment.user | capitalize }}</strong></label>
+                    <div class="dropdown" v-if="user.id == comment.user_id">
+                        <button class="btn btn-sm" type="button" :id="'dropdownMenuButton-'+comment.id" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="ri-more-2-fill"></i>
+                        </button>
+                        <ul class="dropdown-menu" :aria-labelledby="'dropdownMenuButton-'+comment.id">
+                            <a class="dropdown-item" @click="deleteComment(comment.id)">Excluir</a>
+                        </ul>
+                    </div>
+                </div>
+                <p style="white-space:pre-wrap">{{comment.text}}</p>
+                <small class="text-end" >{{comment.date | prettyDate}}</small>
+            </div>
         </div>
         <!-- <reaction-list
             :user-id="userId"
             :item-id="comment.id"
         >
         </reaction-list> -->
-
-        </div>
         <form @submit.prevent="createComment" class="card p-4">
             <h6> Adicionar um Comentários</h6>
             <textarea id="userComment"  name="userComment"
