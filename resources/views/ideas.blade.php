fb6ce4d6eccb4150336be860c8f06e76e76ef3f7
Fernando Bevilacqua <dovyski@gmail.com>
2021-06-30 10:11:21 -0300

    feat: rascunho inicial de ideias voadoras na página de ideias

--- resources/views/ideas.blade.php
+++ resources/views/ideas.blade.php
@@ -43,6 +43,30 @@
         </div>
     </div>
 </section>
+
+<div class="idea-stream">
+    <div id="background-wrap">
+        <div class="x1">
+            <div class="cloud"></div>
+        </div>
+    
+        <div class="x2">
+            <div class="cloud"></div>
+        </div>
+    
+        <div class="x3">
+            <div class="cloud"></div>
+        </div>
+    
+        <div class="x4">
+            <div class="cloud"></div>
+        </div>
+    
+        <div class="x5">
+            <div class="cloud"></div>
+        </div>
+    </div>
+</div>
 @endsection
 
 @section('content')
