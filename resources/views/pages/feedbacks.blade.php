81d2f0291f299df9a82a85a77de58c778a05844b
Fernando Bevilacqua <dovyski@gmail.com>
2021-07-01 12:22:38 -0300

    refactor: remove alguns arquivos que foram migrados

--- resources/views/pages/feedbacks.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.base')
-@section('content')
-<section class="my-5 container">
-    <feedback-page :user="{{Auth()->user()}}" :token="{{$token}}"></feedback-page>
-</section>
-@endsection
-
--- resources/views/pages/servicos-acompanhar.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.base')
-@section('content')
-<section class="my-5 container">
-    <servicos-acompanhar-page :user="{{Auth()->user()}}" :token="{{$token}}"></servicos-acompanhar-page>
-</section>
-@endsection
-
