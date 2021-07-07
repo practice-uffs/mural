8dab103c0c005be8d64d59a1de888ec79ec6378e
Cleisson <cleisson.raimundi@gmail.com>
2021-07-07 10:46:29 -0300

    refactory: alterado para 2 meses duração de token para usuário #339

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -120,7 +120,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth('api')->factory()->getTTL() * 60
+            'expires_in' => auth('api')->factory()->getTTL() * 60 * 24 * 60 // minutos * horas * dias = 2 meses
         ]);
     }
 
