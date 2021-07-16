aca4a6b73c646944903fea6b32508deb220c97c5
Fernando Bevilacqua <dovyski@gmail.com>
2021-07-16 17:44:53 -0300

    feat: preview de imagens no upload de arquivos

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,4 +2,7 @@ require('./bootstrap');
 require('alpinejs');
 
 import * as FilePond from 'filepond';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+
 window.FilePond = FilePond;
+window.FilePondPluginImagePreview = FilePondPluginImagePreview;
