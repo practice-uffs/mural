98070da9a44a2ed535d7f61d12ff14eed2975bd9
Fernando Bevilacqua <dovyski@gmail.com>
2021-07-26 09:08:32 -0300

    feat(api-auth) refatora para usar nova estrutura interna

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -10,71 +10,49 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    const ADMIN_USERS = [
-        'alessandra.pedrotti',
-        'alisson.peloso',
-        'ana.silveira',
-        'andrew.silva',
-        'amanda.bisognin',
-        'brenda.reis',
-        'cleisson.raimundi',
-        'estela.boas',
-        'gessicazanon',
-        'guilherme.graeff',
-        'isabeli.reik',
-        'jean.hilger',
-        'junior.ramisch',
-        'luiz.dameda',
-        'luiza.tonial',
-        'mariaeduarda.santos',
-        'matheus.negrao',
-        'milena.pastorini',
-        'morgana.oliveira',
-        'pedro.maximowski',
-        'raphael.santos',
-        'robison.silva',
-        'stefani.meneghetti',
-        'taynara.christmann',
-        'thalia.friedrich',
-        'vinicius.alves',
-        'vinicius.reis',
-    ];
-        /**
+    /**
      * Get a JWT via given credentials.
      *
      * @return \Illuminate\Http\JsonResponse
      */
     public function login(Request $request)
     {
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
         // Confere se esse usuário existe na base idUFFS
         $credentials_uffs = [
             'user'     => strtolower($request->input('username')),
             'password' => $request->input('password'),
         ];
+
         $auth = new \CCUFFS\Auth\AuthIdUFFS();
         $userData = $auth->login($credentials_uffs);
         
         if (!$userData) {
             return response()->json(['error' => 'usuário ou senha incorreta'], 401);
-        }else{
-            //Gera o token e realiza o Login do usuário
-            $credentials = [
-                'username' => $userData->username,
-                'password' => $userData->pessoa_id,
-            ];
+        }
+
+        //Gera o token e realiza o Login do usuário
+        $credentials = [
+            'username' => $userData->username,
+            'password' => $userData->pessoa_id,
+        ];
+
+        if (!($token = auth('api')->attempt($credentials))) {
+            $userData->password = bcrypt($credentials['password']);
+            // Caso usuário exista na base idUFFS, ele criará o usuário no mural
+            $user = $this->getOrCreateUser($userData);
+            // e tentará gerar o token e fazer login novamente
             if (! $token = auth('api')->attempt($credentials)) {
-                
-                $userData->password = bcrypt($credentials['password']);
-                // Caso usuário exista na base idUFFS, ele criará o usuário no webfeedback
-                $user = $this->getOrCreateUser($userData);
-                // e tentará gerar o token e fazer login novamente
-                if (! $token = auth('api')->attempt($credentials)) {
-                    return response()->json(['error' => 'não autorizado'], 401);
-                }
-                return $this->respondWithToken($token);
+                return response()->json(['error' => 'não autorizado'], 401);
             }
             return $this->respondWithToken($token);
         }
+
+        return $this->respondWithToken($token);
     }
 
     public function me()
@@ -132,15 +110,9 @@ class AuthController extends Controller
             'email' => $data->email,
             'uid' => $data->uid,
             'name' => $data->name,
-            'cpf' => $data->cpf
+            'cpf' => $data->cpf,
+            'type' => $data->type,
         ];
-
-        if (in_array($data['username'], SELF::ADMIN_USERS)) {
-            $data['type'] = User::ADMIN;
-        
-        } else {
-            $data['type'] = User::NORMAL;
-        }
 
         if ($user) {
             $user->update($data);
