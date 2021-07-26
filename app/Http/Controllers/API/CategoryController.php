bdccfdb2f724a18a1cbc2ba3f646e1bccc228aca
Fernando Bevilacqua <dovyski@gmail.com>
2021-07-26 09:16:48 -0300

    feat(api) refatora endpoint de categorias

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
-use App\Category;
+use App\Model\Category;
 
 class CategoryController extends Controller
 {
@@ -19,53 +19,8 @@ class CategoryController extends Controller
     public function index(Request $request)
     {
         return response(
-            Category::where('item_type', $request -> item_type) -> get(),
+            Category::all(),
             Response::HTTP_OK
         );
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
