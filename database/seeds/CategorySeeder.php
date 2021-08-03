e7010cdd3bb710599bf0baebb760540da55eabcc
Fernando Bevilacqua <dovyski@gmail.com>
2021-08-03 11:24:49 -0300

    feat: melhoramentos dos seeds

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -59,5 +59,13 @@ class CategorySeeder extends Seeder
             'icon_svg_path' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />',
             'color' => 'purple-600',
         ]);
+
+        Category::create([
+            'name' => 'Software',
+            'slug' => 'software',
+            'description' => 'Podemos ajudar na criação de softwares acadêmicos, como aplicativos para celular ou sites, tanto para divulgar informações quanto para permitir uma melhor interação.',
+            'icon_svg_path' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />',
+            'color' => 'indigo-500',
+        ]);        
     }
 }
