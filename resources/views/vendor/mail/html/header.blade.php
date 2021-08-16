ad9530fdb518c1b9f3f00f51d08539ac697e88f9
Fernando Bevilacqua <dovyski@gmail.com>
2021-08-16 11:50:49 -0300

    feat: estilos de e-mail

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ asset('img/mail/practice-logo.png') }}" class="logo" alt="Logo do Practice">
+{{-- $slot --}}
+</a>
+</td>
+</tr>
