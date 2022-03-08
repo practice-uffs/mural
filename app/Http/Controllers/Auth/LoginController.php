691c5cd2c2438e5dbb8beb5d47dbbc0f8f01a5ba
Alisson <alisson.luan2000@gmail.com>
2022-03-08 10:44:33 -0300

    fix: ajusta bug nas tentativas de login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -20,9 +20,14 @@ class LoginController extends Controller
             return redirect()->route('home');
         }
 
-        if (empty(session()->get("_loginAttempts"))){
+        $output = new \Symfony\Component\Console\Output\ConsoleOutput();
+
+        if (empty(session()->get("_loginAttempts")) && session()->get("_loginAttempts") !== 0){
             session(["_loginAttempts" => env('LOGIN_ATTEMPTS')]);
+            $output->writeln("Entrou no empty");
         }
+
+        $output->writeln(session()->get("_loginAttempts"));
 
         return view('login');
     }
