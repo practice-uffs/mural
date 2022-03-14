3252a6cf5f922898dddeb69938f1699f9ba7c3f9
Stefani Meneghetti <51202548+stefanimeneghetti@users.noreply.github.com>
2022-03-14 11:57:56 -0300

    feat: ajustes para versao mobile (#439)
    * feat: ajustes para versao mobile
    
    * fix: corrige bug no campo de perguntas adicionais
    
    Co-authored-by: Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>

--- resources/views/order/show.blade.php
+++ resources/views/order/show.blade.php
@@ -9,11 +9,11 @@
         </header>
 
         <div class="row">
-            <div class="col-9">
+            <div class="col-lg-9 order-2 order-md-1 col-md-8">
                 @livewire('order.show', ['order' => $order])
                 @livewire('comments', ['commentable' => $order])
             </div>
-            <div class="col-3">
+            <div class="col-lg-3 order-1 order-md-2 col-md-4">
                 @livewire('order.status', ['order' => $order])
             </div>
         </div>
