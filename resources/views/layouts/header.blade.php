56cc9c3cdac3f8f00d29bc97971a23c41bd3478b
Stefani Meneghetti <meneghettistefani@gmail.com>
2022-03-29 09:32:13 -0300

    fix: ajustes feitos

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -29,25 +29,25 @@
             </ul>
         </nav>
 
-        <nav style="left: 40%" class="lg:invisible absolute left-2 w-full">
+        <nav class="lg:invisible absolute left-1 w-full d-flex justify-content-end">
             @admin
                 <li class="dropdown ml-3">
                     <div tabindex="0" class="btn btn-primary btn-outline" data-bs-toggle="dropdown" aria-expanded="false">Admin <i class="bi bi-chevron-down"></i></div>
                     <ul class="shadow dropdown-menu bg-base-100 rounded-box w-52">
-                        <li class="p-3 dropdown-item"><a href="{{ route('admin.orders') }}">Pedidos</a></li> 
+                        <li class="dropdown-item"><a class="nav-link" href="{{ route('admin.orders') }}">Pedidos</a></li> 
                         <li class="dropdown-item"><hr /></li> 
-                        <li class="p-3 dropdown-item"><a href="{{ route('admin.service') }}">Serviços</a></li> 
-                        <li class="p-3 dropdown-item"><a href="{{ route('admin.category') }}">Categorias</a></li> 
-                        <li class="p-3 dropdown-item"><a href="{{ route('admin.location') }}">Locais</a></li> 
+                        <li class="dropdown-item"><a class="nav-link" href="{{ route('admin.service') }}">Serviços</a></li> 
+                        <li class="dropdown-item"><a class="nav-link" href="{{ route('admin.category') }}">Categorias</a></li> 
+                        <li class="dropdown-item"><a class="nav-link" href="{{ route('admin.location') }}">Locais</a></li> 
                         <li class="dropdown-item"><hr /></li> 
-                        <li class="p-3 dropdown-item"><a href="{{ route('admin.user') }}">Usuários</a></li> 
-                        <li class="p-3 dropdown-item"><a href="{{ route('admin.subscriber') }}">Newsletter</a></li>
+                        <li class="dropdown-item"><a class="nav-link" href="{{ route('admin.user') }}">Usuários</a></li> 
+                        <li class="dropdown-item"><a class="nav-link" href="{{ route('admin.subscriber') }}">Newsletter</a></li>
                     </ul>
                 </li>
             @endadmin
-            <li class="dropdown mx-2">
+            <li class="dropdown mx-2 pr-2">
                 <div tabindex="0" class="btn btn-primary" data-bs-toggle="dropdown" aria-expanded="false">Menu <i class="bi bi-chevron-down"></i></div>
-                <ul class="dropdown-menu">
+                <ul class="dropdown-menu rounded-box">
                     @include('header-menu')
                 </ul>
             </li>
