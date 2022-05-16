1a5300b87b0ee0cb90ad0e60d3e041e6db6d1d33
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2022-05-16 08:12:05 -0300

    feat: ajusta hero-img

--- resources/views/admin/service.blade.php
+++ resources/views/admin/service.blade.php
@@ -1,32 +1,45 @@
 @extends('layouts.base')
 @section('content')
-
-<section id="hero" class="hero hero-slim d-flex align-items-center">
-    <div class="container">
-        <div class="row">
-            <div class="col-4 hero-img">
-                <img src="{{ asset('img/undraw.co/online_groceries.svg') }}" class="w-64 h-auto" alt="">
-            </div>
-            <div class="col-8 d-flex flex-column justify-content-center">
-                <h1>Serviços</h1>
-                <h2>Gerenciamento dos serviços disponíveis para solicitação dos clientes.</h2>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section>
-    <div class="container">
-        <header class="section-header">
-            <h2>Criar um novo serviço</h2>
-        </header>
-
-        <div class="row">
-            <div class="col-12">
-                @livewire('admin.service', [
-                    'model' => 'App\Models\Service'
-                ])
+    <section id="hero" class="hero hero-slim d-flex align-items-center h-auto">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 hero-img" data-aos="zoom-out" data-aos-delay="200">
+                    <img src="{{ asset('img/undraw.co/online_groceries.svg') }}" class="w-75 h-auto mx-auto img-fluid"
+                        alt="">
+                </div>
+                <div class="col-lg-8 d-flex flex-column justify-content-center my-3">
+                    <h1 data-aos="fade-up">Serviços</h1>
+                    <h2 data-aos="fade-up" data-aos-delay="400">Gerenciamento dos serviços disponíveis para solicitação dos
+                        clientes.</h2>
+                </div>
             </div>
         </div>
-    </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <header class="section-header">
+                <h2>Criar um novo serviço</h2>
+            </header>
+
+            <div class="row">
+                <div class="col-12">
+                    @livewire('admin.service', [
+                        'model' => 'App\Models\Service',
+                    ])
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('styles')
+    <style>
+        section#hero {
+            min-height: 50vh;
+            padding: 0 0 0 0;
+            margin-bottom: 50px
+        }
+
+    </style>
 @endsection
