b79c26259955b6306bfc0980ce99844bd05fcb69
Alisson Luan de Lima Peloso <51202705+alissonpeloso@users.noreply.github.com>
2022-05-16 11:25:38 -0300

    fix: hero-img Admin/Services (#476)
    * feat: ajusta hero-img
    
    * feat: fix hero-img on Admin/Categorias, Admin/Locais and Admin/Usuários
    
    * fix: rm delay effect

--- resources/views/admin/location.blade.php
+++ resources/views/admin/location.blade.php
@@ -1,32 +1,45 @@
 @extends('layouts.base')
 @section('content')
-
-<section id="hero" class="hero hero-slim d-flex align-items-center">
-    <div class="container">
-        <div class="row">
-            <div class="col-4 hero-img">
-                <img src="{{ asset('img/undraw.co/destinations.svg') }}" class="w-64 h-auto" alt="">
-            </div>
-            <div class="col-8 d-flex flex-column justify-content-center">
-                <h1>Locais</h1>
-                <h2>Gerenciamento de locais mostrados nas solicitações de serviço.</h2>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section>
-    <div class="container">
-        <header class="section-header">
-            <h2>Criar um novo local</h2>
-        </header>
-
-        <div class="row">
-            <div class="col-12">
-                @livewire('crud.main', [
-                    'model' => 'App\Models\Location'
-                ])
+    <section id="hero" class="hero hero-slim d-flex align-items-center h-auto">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 hero-img">
+                    <img src="{{ asset('img/undraw.co/destinations.svg') }}" class="w-75 h-auto mx-auto img-fluid" alt="">
+                </div>
+                <div class="col-lg-8 d-flex flex-column justify-content-center my-3">
+                    <h1>Locais</h1>
+                    <h2>Gerenciamento de locais mostrados nas solicitações de
+                        serviço.</h2>
+                </div>
             </div>
         </div>
-    </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <header class="section-header">
+                <h2>Criar um novo local</h2>
+            </header>
+
+            <div class="row">
+                <div class="col-12">
+                    @livewire('crud.main', [
+                        'model' => 'App\Models\Location',
+                    ])
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+
+@section('styles')
+    <style>
+        section#hero {
+            min-height: 50vh;
+            padding: 0 0 0 0;
+            margin-bottom: 50px
+        }
+
+    </style>
 @endsection
