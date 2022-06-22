3ff93024be6071cca396ecba56a06b6092e8d864
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2022-06-22 17:03:41 -0300

    fix: ajusta design da mensagem de sucesso

--- resources/views/livewire/crud/success.blade.php
+++ resources/views/livewire/crud/success.blade.php
@@ -1,14 +1,15 @@
 <div>
-    <div class="alert alert-success w-1/2 m-auto mt-5 @if (!$show_success || !$finished) hidden @endif">
+    <div class="alert alert-success w-100 m-auto mt-5 flex-column @if (!$show_success || !$finished) hidden @endif">
         <div class="flex-1">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
             <label class="ml-3 mt-1"><strong>Pronto!</strong> Sua ação foi registrada com sucesso.</label>
         </div>
 
-        <div class="flex-none">
-            <button class="btn btn-sm btn-success pt-0" wire:click="finished(false)">Ok</button>
+        <div class="flex-1 mt-3">
+            <button class="btn btn-sm btn-success px-4" wire:click="finished(false)">Ok</button>
         </div>
     </div>
 </div>
