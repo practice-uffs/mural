9c3a6951647762c498251229d7d1540d791db69e
Adriano <zorteaadriano@gmail.com>
2022-11-10 12:22:14 -0300

    feat: #523 configurando google analytics

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -88,7 +88,15 @@
 
         <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-SYT1Y1FGPE"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
+            gtag('config', 'G-SYT1Y1FGPE');
+        </script>
 
 
         <!-- Vendor JS Files -->
